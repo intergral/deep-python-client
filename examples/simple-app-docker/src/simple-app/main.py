bc7fac486824f0d4a443c1d2d590d1594a5e226f
Ben Donnelly <ben_donnelly@intergral.com>
2023-03-31 16:34:07 +0100

    fix: test apps not handling signals for shutdown

--- examples/simple-app-docker/src/simple-app/main.py
+++ examples/simple-app-docker/src/simple-app/main.py
@@ -11,16 +11,28 @@
 #     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
-
+import signal
 import time
 
 import deep
 from simple_test import SimpleTest
 
 
+class GracefulKiller:
+    kill_now = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, *args):
+        self.kill_now = True
+
+
 def main():
+    killer = GracefulKiller()
     ts = SimpleTest("This is a test")
-    while True:
+    while not killer.kill_now:
         try:
             ts.message(ts.new_id())
         except BaseException as e:
