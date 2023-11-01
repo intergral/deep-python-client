f3c3827a86b02ed8d68a334dd196b4b140b2ce48
Ben Donnelly <ben_donnelly@intergral.com>
2023-11-01 17:42:45 +0000

    feat(api): add api function to register tracepoint directly

--- src/deep/api/deep.py
+++ src/deep/api/deep.py
@@ -9,9 +9,13 @@
 #      but WITHOUT ANY WARRANTY; without even the implied warranty of
 #      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #      GNU Affero General Public License for more details.
+from typing import Dict, List
 
 from deep.api.plugin import load_plugins
 from deep.api.resource import Resource
+from deep.api.tracepoint import TracePointConfig
+from deep.config import ConfigService
+from deep.config.tracepoint_config import TracepointConfigService
 from deep.grpc import GRPCService
 from deep.poll import LongPoll
 from deep.processor import TriggerHandler
@@ -25,7 +29,7 @@ class Deep:
     DEEP is so small there is no need for service injection work.
     """
 
-    def __init__(self, config):
+    def __init__(self, config: 'ConfigService'):
         self.started = False
         self.config = config
         self.grpc = GRPCService(self.config)
@@ -51,3 +55,27 @@ class Deep:
             return
         self.task_handler.flush()
         self.started = False
+    def register_tracepoint(self, path: str, line: int, args: Dict[str, str] = None,
+                            watches: List[str] = None) -> 'TracepointRegistration':
+        if watches is None:
+            watches = []
+        if args is None:
+            args = {}
+        tp_config = self.config.tracepoints.add_custom(path, line, args, watches)
+        return TracepointRegistration(tp_config, self.config.tracepoints)
+
+
+class TracepointRegistration:
+    _cfg: TracePointConfig
+    _tpServ: TracepointConfigService
+
+    def __init__(self, cfg: TracePointConfig, tracepoints: TracepointConfigService):
+        self._cfg = cfg
+        self._tpServ = tracepoints
+
+    def get(self) -> TracePointConfig:
+        return self._cfg
+
+    def unregister(self):
+        self._tpServ.remove_custom(self._cfg)
+
